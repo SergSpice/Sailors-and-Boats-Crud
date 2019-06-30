7d354120c7c4bbdccf1cab99860180c678596b47
Nicolás Ruiz <nicolasruiz81@gmail.com>
2019-06-29 20:53:30 -0400

    añadido algunos comentarios en el sql

--- backend/migrations/sailors_data.sql
+++ backend/migrations/sailors_data.sql
@@ -1,3 +1,4 @@
+/* Tabla Sailors */
 insert into sailors (id, sname, rating, age) values (22, 'Dustin', 7, 45.0);
 insert into sailors (id, sname, rating, age) values (29, 'Brutus', 1, 33.0);
 insert into sailors (id, sname, rating, age) values (31, 'Lubber', 8, 55.5);
@@ -8,12 +9,12 @@ insert into sailors (id, sname, rating, age) values (71, 'Zorba', 10, 16.0);
 insert into sailors (id, sname, rating, age) values (74, 'Horatio', 9, 35.0);
 insert into sailors (id, sname, rating, age) values (85, 'Art', 3, 25.5);
 insert into sailors (id, sname, rating, age) values (95, 'Bob', 3, 63.5);
-
+/* Tabla Boats */
 insert into boats (id, bname, color) values (101, 'Interlake', 'blue');
 insert into boats (id, bname, color) values (102, 'Interlake', 'red');
 insert into boats (id, bname, color) values (103, 'Clipper', 'green');
 insert into boats (id, bname, color) values (104, 'Marine', 'red');
-
+/* Tabla Reserves */
 insert into reserves (sid, bid, day) values (22, 101, '907988400000');
 insert into reserves (sid, bid, day) values (22, 102, '907988400000');
 insert into reserves (sid, bid, day) values (22, 103, '907815600000');
